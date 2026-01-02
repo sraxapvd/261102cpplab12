d0d96a83285f514562a2a618fc9b7bded03ed660
Rada <sirada_dee@cmu.ac.th>
2026-01-02 14:23:37 +0700

    lab12_1.cpp completed 1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int x) ;
+long long fibonacci(int x) ;
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int x)
+long long fibonacci(int x)
 {
     if (x == 0)
         return 0;
